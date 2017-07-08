b0d9e021ae5d823a6e47a26b65b2cb8227649726
Sachin-Radhakrishnan <sachin.r@gadgeon.com>
2017-07-08 10:40:27 +0530

    changes as on saturday

--- Dump20170708.sql
+++ Dump20170708.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 5.7.18, for Linux (x86_64)
 --
--- Host: 127.0.0.1    Database: database
+-- Host: localhost    Database: database
 -- ------------------------------------------------------
 -- Server version	5.7.18-0ubuntu0.17.04.1
 
@@ -14,6 +14,33 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `category` (
+  `category_id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_name` varchar(50) NOT NULL,
+  `status` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`category_id`),
+  FULLTEXT KEY `category_name` (`category_name`),
+  FULLTEXT KEY `category_name_2` (`category_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES (1,'Laptops','active'),(2,'Mobiles','active'),(3,'Televisions','active'),(4,'Clothing','active'),(5,'Life Style Accessories','active'),(6,'Foot wears','active');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `order`
@@ -215,4 +242,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-07-08  8:27:01
+-- Dump completed on 2017-07-08 10:37:12
