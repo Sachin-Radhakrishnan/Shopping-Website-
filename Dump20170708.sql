ded46b63543ac6619ea3e01864955756df21347d
Sachin-Radhakrishnan <sachin.r@gadgeon.com>
2017-07-13 20:53:06 +0530

    changes on thursday

--- Dump20170708.sql
+++ Dump20170708.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 5.7.18, for Linux (x86_64)
 --
--- Host: localhost    Database: database
+-- Host: 127.0.0.1    Database: database
 -- ------------------------------------------------------
 -- Server version	5.7.18-0ubuntu0.17.04.1
 
@@ -25,7 +25,7 @@ DROP TABLE IF EXISTS `category`;
 CREATE TABLE `category` (
   `category_id` int(11) NOT NULL AUTO_INCREMENT,
   `category_name` varchar(50) NOT NULL,
-  `status` varchar(10) DEFAULT NULL,
+  `status` varchar(10) DEFAULT 'active',
   PRIMARY KEY (`category_id`),
   FULLTEXT KEY `category_name` (`category_name`),
   FULLTEXT KEY `category_name_2` (`category_name`)
@@ -38,34 +38,91 @@ CREATE TABLE `category` (
 
 LOCK TABLES `category` WRITE;
 /*!40000 ALTER TABLE `category` DISABLE KEYS */;
-INSERT INTO `category` VALUES (1,'Laptops','active'),(2,'Mobiles','active'),(3,'Televisions','active'),(4,'Clothing','active'),(5,'Life Style Accessories','active'),(6,'Foot wears','active');
+INSERT INTO `category` VALUES (1,'Laptops','active'),(2,'Mobile','active'),(3,'Televisions','active'),(4,'Clothing','active'),(5,'Life Style Accessories','active'),(6,'shoes','active');
 /*!40000 ALTER TABLE `category` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `order`
+-- Table structure for table `employee`
 --
 
-DROP TABLE IF EXISTS `order`;
+DROP TABLE IF EXISTS `employee`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `order` (
-  `order_id` int(10) unsigned NOT NULL,
-  `shipping_id` int(11) DEFAULT NULL,
-  `price` bigint(1) DEFAULT NULL,
-  `date_created` datetime DEFAULT NULL,
-  `confirmation_no` tinytext,
-  PRIMARY KEY (`order_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `employee` (
+  `employee_id` int(11) NOT NULL AUTO_INCREMENT,
+  `fname` varchar(45) NOT NULL,
+  `lname` varchar(45) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `salary` bigint(10) NOT NULL,
+  `hire_date` varchar(225) NOT NULL,
+  `status` varchar(15) DEFAULT 'active',
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`employee_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `order`
+-- Dumping data for table `employee`
 --
 
-LOCK TABLES `order` WRITE;
-/*!40000 ALTER TABLE `order` DISABLE KEYS */;
-/*!40000 ALTER TABLE `order` ENABLE KEYS */;
+LOCK TABLES `employee` WRITE;
+/*!40000 ALTER TABLE `employee` DISABLE KEYS */;
+INSERT INTO `employee` VALUES (7,'fdfsdf','fsfsdf','Male',1234,'Mon Jul 10 2017','active',31),(8,'sachin','Kurup','Male',1234,'Wed Jul 12 2017','active',32);
+/*!40000 ALTER TABLE `employee` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ordered_products`
+--
+
+DROP TABLE IF EXISTS `ordered_products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ordered_products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `order_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ordered_products`
+--
+
+LOCK TABLES `ordered_products` WRITE;
+/*!40000 ALTER TABLE `ordered_products` DISABLE KEYS */;
+INSERT INTO `ordered_products` VALUES (7,43,4),(8,44,5),(9,45,4),(10,46,2),(11,47,2),(12,48,5);
+/*!40000 ALTER TABLE `ordered_products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `orders` (
+  `order_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(45) NOT NULL,
+  `shipping_id` varchar(45) NOT NULL,
+  `total` varchar(45) NOT NULL,
+  `status` varchar(45) NOT NULL,
+  `date_added` varchar(45) NOT NULL,
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=49 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+INSERT INTO `orders` VALUES (43,'19','46','1263','pending','7-12-2017'),(44,'19','47','392','pending','7-12-2017'),(45,'19','48','1263','pending','7-12-2017'),(46,'19','49','550','pending','7-13-2017'),(47,'19','50','550','pending','7-13-2017'),(48,'19','51','392','pending','7-13-2017');
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -96,7 +153,7 @@ CREATE TABLE `product` (
 
 LOCK TABLES `product` WRITE;
 /*!40000 ALTER TABLE `product` DISABLE KEYS */;
-INSERT INTO `product` VALUES (1,'Action Shoes','. • Eloquent style • Lightweight high finish PU sole • Extra Comfort • Well-cushioned insole and a shock-absorbing heel',1099,0,'active',1),(2,' Imperial Analog Watch','Water Resistant,Display Type: Analog Strap: Black',545,15,'active',3),(3,'Lotto Men Brown Beige Sandals','Crafted from a rich quality material, this pair of floater sandals promises to keep your feet at ease all day long. ',325,0,'active',2),(4,'HP 15.6 inch Laptop Backpack ','Laptop Backpack,3 Compartments,Material: Polyester,Used For Laptop,Waterproof',1250,11,'active',4),(5,'Styylo Fashion Alloy Jewel Set  (Gold)','Earring & Necklace Set;For Women, Girls;Yellow Gold Plated,Made of Alloy,Color: Gold',388,11,'active',5),(6,'HP Laptops','Intel Core i3 Processor (6th Gen),4 GB DDR4 RAM,64 bit Windows 10 Operating System,1 TB HDD,14 inch Display',32990,15,'active',13),(7,'Acer','HD LED Backlit Display,Intel Core i3 Processor (6th Gen),4 GB DDR4 RAM,64 bit Windows 10 Operating System,1 TB HDD,15.6 inch Display',30990,10,'active',13),(8,'Dell Laptops','Intel Core i3 Processor (6th Gen),4 GB DDR4 RAM,64 bit Windows 10 Operating System,1 TB HDD,14 inch Display',31990,19,'active',13),(9,'Lenovo','Free Pre Loaded MS Office Home & Student 2016,Intel Core i3 Processor (6th Gen),4 GB DDR4 RAM,64 bit Windows 10 Operating System,1 TB HDD,14 inch Display',33000,10,'active',13),(10,'Micromax Canvas','Intel Core i3 Processor (5th Gen),4 GB DDR3 RAM,64 bit Windows 10 Operating System,1 TB HDD,15.6 inch Display',32990,12,'active',13),(11,'iBall Laptops','Backlit Display,Intel Core i3 Processor (6th Gen),4 GB DDR4 RAM,64 bit Windows 10 Operating System,1 TB HDD,15.6 inch Display',28325,15,'active',13);
+INSERT INTO `product` VALUES (1,'Action Shoes','Eloquent style,\nLightweight high finish PU sole ,\nExtra Comfort ,\nWell-cushioned insole and a shock-absorbing heel',1099,11,'active',1),(2,' Imperial Analog Watch','Water Resistant, Display Type: Analog Strap, Black',545,11,'active',3),(3,'Lotto Men Brown Beige Sandals','Crafted from a rich quality material, this pair of floater sandals promises to keep your feet at ease all day long. ',325,13,'active',2),(4,'HP 15.6 inch Laptop Backpack ','Laptop Backpack,3 Compartments,Material: Polyester,Used For Laptop,Waterproof',1250,7,'active',4),(5,'Styylo Fashion Alloy Jewel Set  (Gold)','Earring & Necklace Set;For Women, Girls;Yellow Gold Plated,Made of Alloy,Color: Gold',388,11,'active',5),(6,'HP Laptops','Intel Core i3 Processor (6th Gen),4 GB DDR4 RAM,64 bit Windows 10 Operating System,1 TB HDD,14 inch Display',32990,12,'active',13),(7,'Acer','HD LED Backlit Display,Intel Core i3 Processor (6th Gen),4 GB DDR4 RAM,64 bit Windows 10 Operating System,1 TB HDD,15.6 inch Display',30990,17,'active',13),(8,'Dell Laptops','Intel Core i3 Processor (6th Gen),4 GB DDR4 RAM,64 bit Windows 10 Operating System,1 TB HDD,14 inch Display',31990,17,'active',13),(9,'Lenovo','Free Pre Loaded MS Office Home & Student 2016,Intel Core i3 Processor (6th Gen),4 GB DDR4 RAM,64 bit Windows 10 Operating System,1 TB HDD,14 inch Display',33000,13,'active',13),(10,'Micromax Canvas','Intel Core i3 Processor (5th Gen),4 GB DDR3 RAM,64 bit Windows 10 Operating System,1 TB HDD,15.6 inch Display',32990,12,'active',13),(11,'iBall Laptop','Backlit Display,Intel Core i3 Processor (6th Gen),4 GB DDR4 RAM,64 bit Windows 10 Operating System,1 TB HDD,15.6 inch Display',28325,200,'active',13);
 /*!40000 ALTER TABLE `product` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -109,13 +166,15 @@ DROP TABLE IF EXISTS `shippingDetails`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `shippingDetails` (
   `shipping_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
   `fname` varchar(45) NOT NULL,
   `lname` varchar(45) NOT NULL,
   `address` varchar(25) CHARACTER SET utf8 NOT NULL,
   `state` varchar(45) NOT NULL,
   `zip` int(11) NOT NULL,
+  `city` varchar(45) NOT NULL,
   PRIMARY KEY (`shipping_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -124,6 +183,7 @@ CREATE TABLE `shippingDetails` (
 
 LOCK TABLES `shippingDetails` WRITE;
 /*!40000 ALTER TABLE `shippingDetails` DISABLE KEYS */;
+INSERT INTO `shippingDetails` VALUES (46,19,'eersef','dsfsdsd','gdsfgsdag','cvxbxcb',121321,'gdsgds'),(47,19,'eersef','dsfsdsd','gdsfgsdag','cvxbxcb',121321,'gdsgds'),(48,19,'eersef','dsfsdsd','gdsfgsdag','cvxbxcb',121321,'gdsgds'),(49,19,'dasddas','dssfdF','SFSDFSD','SDFSDFSDF',122321,'DSFSDFSD'),(50,19,'dasddas','dssfdF','SFSDFSD','SDFSDFSDF',122321,'DSFSDFSD'),(51,19,'dasddas','dssfdF','SFSDFSD','SDFSDFSDF',122321,'DSFSDFSD');
 /*!40000 ALTER TABLE `shippingDetails` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -167,7 +227,7 @@ CREATE TABLE `subcategory` (
   PRIMARY KEY (`subcategory_id`),
   FULLTEXT KEY `subcategory_name` (`subcategory_name`),
   FULLTEXT KEY `subcategory_name_2` (`subcategory_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -176,7 +236,7 @@ CREATE TABLE `subcategory` (
 
 LOCK TABLES `subcategory` WRITE;
 /*!40000 ALTER TABLE `subcategory` DISABLE KEYS */;
-INSERT INTO `subcategory` VALUES (1,'Shoes',6,'active'),(2,'Sandals',6,'active'),(3,'Watches',5,'active'),(4,'Bags',4,'active'),(5,'Jewellery',4,'active'),(6,'Men T- Shirts',4,'active'),(7,'Men Jeans',4,'active'),(8,'Sarees',4,'active'),(9,'Churidars',4,'active'),(10,'Baby Frocks',4,'active'),(11,'LED TV',3,'active'),(12,'40 Inches TV',3,'active'),(13,'Branded Laptops',1,'active'),(14,'Laptop Accessories',1,'active'),(15,'Android Mobiles',2,'active'),(16,'iPhones',2,'active');
+INSERT INTO `subcategory` VALUES (1,'Shoes',6,'active'),(2,'Sandals',6,'active'),(3,'Watches',5,'active'),(4,'Bags',4,'active'),(5,'Jewellery',4,'active'),(6,'Men T- Shirts',4,'active'),(7,'Men Jeans',4,'active'),(8,'Sarees',4,'active'),(9,'Churidars',4,'active'),(10,'Baby Frocks',4,'active'),(11,'LED TV',3,'active'),(12,'40 Inches TV',3,'active'),(13,'Branded Laptops',1,'active'),(14,'Laptop Accessories',1,'active'),(15,'Android Mobiles',2,'active'),(16,'iPhones',2,'active'),(29,'desks',20,'inactive');
 /*!40000 ALTER TABLE `subcategory` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -215,12 +275,13 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(15) NOT NULL,
+  `username` varchar(15) DEFAULT NULL,
   `email` varchar(45) DEFAULT NULL,
-  `password` varchar(45) DEFAULT NULL,
-  `status` varchar(45) DEFAULT NULL,
+  `password` varchar(355) DEFAULT NULL,
+  `status` varchar(45) DEFAULT 'active',
+  `usertype` varchar(45) NOT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -229,7 +290,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (19,'sachin','sachin.r@gadgeon.com','Sachin@123',NULL),(21,'saswer','sachin.kurup02@gmail.com','Sachin@123',NULL),(22,'sdfrtt','something@mailinator.com',NULL,NULL);
+INSERT INTO `users` VALUES (19,'sachin','sach@gadgeon.com','sha1$4876b616$1$ccacc2f888f06d43a45014a8c4340c9b1d826cbb','active','user'),(22,'sdfrtt','something@mailinator.com','abc@123','active','executive'),(32,'sachin Kurup','sachin.r@gadgeon.com','sha1$003eb0dc$1$71a51150ced8c1e01b63a5be4d5a8f10e8eba3fb','active','executive'),(33,'admin','admin@gmail.com','sha1$003eb0dc$1$71a51150ced8c1e01b63a5be4d5a8f10e8eba3fb','active','admin');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -242,4 +303,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-07-08 10:37:12
+-- Dump completed on 2017-07-13 20:51:36
